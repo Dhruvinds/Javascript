d8ea1ecb241a5a7b8d8d9de3d186bdbfe77155d7
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-01-14 22:55:48 -0500

    few revise!

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello!")
+// console.log("Hello!")
+
+// let name = "Dhruvin";
+// let empty = "";
+// let space = " ";
+// console.log(name.length);
+// console.log(name[0]);
+// console.log(name[name.length-1]);
+// console.log(empty.length);
+// console.log(space.length);
